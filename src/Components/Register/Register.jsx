7b4e3686514e603d32bf27023d672c35fefde25b
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-04-24 15:38:51 +0600

    register page created

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,7 +1,32 @@
 const Register = () => {
   return (
-    <div>
-      <h2 className="text-3xl">This is register page</h2>
+    <div className="">
+      <div className="mx-auto md:w-1/2">
+        <h2 className="text-3xl mb-4">Please Register</h2>
+        <form>
+          <input
+            placeholder="Email"
+            className="mb-4 w-3/4 py-2 px-4"
+            type="email"
+            name="email"
+            id=""
+          />
+          <br />
+          <input
+            placeholder="Password"
+            className="mb-4 w-3/4 py-2 px-4"
+            type="password"
+            name="password"
+            id=""
+          />
+          <br />
+          <input
+            className="mb-4 w-3/4 btn btn-secondary"
+            type="submit"
+            value="Register"
+          />
+        </form>
+      </div>
     </div>
   );
 };
